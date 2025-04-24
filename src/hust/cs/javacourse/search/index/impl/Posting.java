c8e0ed87d2ddfa2b4225047c42da9ede1993b61d
mango <junde@hust.edu.cn>
2025-04-24 21:26:29 +0800

    fixed posting

--- src/hust/cs/javacourse/search/index/impl/Posting.java
+++ src/hust/cs/javacourse/search/index/impl/Posting.java
@@ -1,8 +1,13 @@
 package hust.cs.javacourse.search.index.impl;
 
 import hust.cs.javacourse.search.index.AbstractPosting;
+
 import java.util.ArrayList; 
+import java.util.Collections;
 import java.util.List;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class Posting extends AbstractPosting{
 
@@ -46,4 +51,96 @@ public class Posting extends AbstractPosting{
     public String toString(){
         return String.format("Posting{docId = %d, freq = %d positions = %s}", this.docId, this.freq, this.positions.toString());
     }
+    /*
+     * 返回包含单词的文档id
+     * @return : doc id
+     */
+    public int getDocId(){
+        return this.docId;
+    }
+    /**
+     * 设置包含单词的文档id
+     * @param docId：包含单词的文档id
+     */
+    public void setDocId(int docId){
+        this.docId = docId;
+    }
+    /**
+     * 返回单词在文档里出现的次数
+     * @return ：出现次数
+     */
+    public int getFreq(){
+        return this.freq;
+    }
+    /**
+     * 设置单词在文档里出现的次数
+     * @param freq:单词在文档里出现的次数
+     */
+    public void setFreq(int freq){
+        this.freq = freq;
+    }
+
+    /**
+     * 返回单词在文档里出现的位置列表
+     * @return ：位置列表
+     */
+    public List<Integer> getPositions(){
+        return positions;
+    }
+    /**
+     * 设置单词在文档里出现的位置列表
+     * @param positions：单词在文档里出现的位置列表
+     */
+    public void setPositions(List<Integer> positions){
+        this.positions = positions;
+    }
+    /**
+     * 比较二个Posting对象的大小（根据docId）
+     * @param o： 另一个Posting对象
+     * @return ：二个Posting对象的docId的差值
+     */
+    @Override
+    public int compareTo(AbstractPosting o){
+        return this.docId - ((Posting)o).docId;
+    }
+    /**
+     * 对内部positions从小到大排序
+     */
+    public void sort(){
+        Collections.sort(this.positions);
+    }
+    
+    /**
+     * 文件序列化写
+     * @param out: 输出流
+     */
+    public void writeObject(ObjectOutputStream out){
+        try {
+            out.writeObject(this.docId);
+            out.writeObject(this.freq);
+            out.writeObject(this.positions);
+        } catch (IOException e) {
+            System.out.println("IO error occured.failed to write posting" + e.getMessage());
+        }
+    }
+    /**
+     * 从二进制文件读
+     * @param in ：输入流对象
+     */
+    public void readObject(ObjectInputStream in){
+        try{
+            this.docId = (int)in.readObject();
+            this.freq = (int)in.readObject();
+            this.positions= (ArrayList<Integer>)in.readObject();
+        }catch(IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
+
 }
